ffcbe9d5334bb7b6373b3cf986ecff39576af6b6
Abhishek sahu <58635182+abhi-dev91@users.noreply.github.com>
2023-03-26 13:19:25 +0530

    Create slave.sh

---
+++ slave.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+apt-get update
+apt-get install python3-pip -y
+pip3 install locust
+ulimit -n 65535
+
+cat >> /etc/security/limits.conf <<EOF
+* soft nofile 65535
+* hard nofile 65535
+EOF
+
+
+cat >> script.py <<EOF
+import gevent
+from locust.contrib.fasthttp import FastHttpUser
+from locust import HttpUser, task
+import resource
+
+
+class MyUser(FastHttpUser):
+  @task
+  def t(self):
+      def concurrent_request(url):
+          self.client.get(url)
+
+      pool = gevent.pool.Pool()
+      urls = ["/"]
+      for url in urls:
+          pool.spawn(concurrent_request, url)
+      pool.join()
+EOF
+
+locust -f script.py --worker --master-host=<IP_OF_MASTER>
